f6ff4f438e47cb5164d3b8522ba9712e80e20bb5
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-04-07 22:52:22 -0300

    (add): adding login page

--- be-the-hero-web/src/App.js
+++ be-the-hero-web/src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
+import Routes from "./routes"
+import "./global.css";
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+  return <Routes />;
 }
 
 export default App;
